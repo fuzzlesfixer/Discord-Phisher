9473962cf06efc86baa4adf7e2a41ab6aadf6408
fuzzlesfixer <133809139+fuzzlesfixer@users.noreply.github.com>
2023-11-17 11:07:33 -0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 from rich.console import Console
 from typing import Literal, Optional
 
-
+# i found this in class frim fuzzles
 cmd = Console()
 
 with open("config.json") as r:
